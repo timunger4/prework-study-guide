bf93e750451efea546a406a430c0d7f9f4cea5a6
timunger4 <timunger4@gmail.com>
2024-12-05 20:19:51 -0800

    prework topics in java randomized

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,38 @@
-const topic = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let x = 0; x < topic.length; x++) {
-    console.log(topic[x]);
+
+function listTopics() { 
+    for (let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML') {
-    console.log("Let's study HMTL!");
-}  else if (topic === 'CSS') {
+function selectTopic() {
+ if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+}  else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-}  else if (topic === 'Git') {
+}  else if (randomTopic === 'Git') {
         console.log("Let's study Git!");
-}  else if (topic === 'JavaScript') {
+}  else if (randomTopic === 'JavaScript') {
     console.log("Let's study JavaScript!");
 }  else {
     console.log('Please try again!');
 }
-*/
+}
 
+console.log('Here are the topics we learned through Prework:')
+listTopics()
+console.log('Which topic should we study first?');
+selectTopic()
+
+
+
+/*
 var shapes =["triangle", "square", "pentagon", "circle"];
 
 for (var x = 0; x < shapes.length; x++) {
     console.log(shapes[x]);
 }
+    */
